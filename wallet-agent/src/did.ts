c51330d84ff483136699ec6f8927e1525db475b7
WWW.QUORUMSTATE.INGERNATIONAL <admin@quorumstate.international>
2026-05-07 16:24:09 +0700

    Update did.ts

--- wallet-agent/src/did.ts
+++ wallet-agent/src/did.ts
@@ -1,12 +1,17 @@
-import * as ed from '@noble/ed25519';
 import { sha256 } from 'js-sha256';
 
-export async function createDid(publicKeyHex: string, controller: string) {
+export function createDid(publicKeyHex: string, controller: string) {
   const did = `did:stg:${sha256(publicKeyHex).slice(0,16)}`;
   const doc = {
+    "@context": ["https://www.w3.org/ns/did/v1"],
     id: did,
     controller,
-    verificationMethod: [{ id: `${did}#key-1`, type: 'Ed25519VerificationKey2018', publicKeyHex }]
+    verificationMethod: [{
+      id: `${did}#key-1`,
+      type: "Ed25519VerificationKey2018",
+      controller: did,
+      publicKeyHex
+    }]
   };
   return { did, doc };
 }
